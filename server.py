7c0f7af508fd48e9c1a5a877c57a31146044e5b2
7ound <7oundabout@gmail.com>
2021-07-29 23:44:07 +0300

    Fran Bow Multiplayer

---
+++ server.py
@@ -0,0 +1,54 @@
+import socket
+from _thread import *
+import sys 
+from settings import WIDTH, HEIGHT
+from player import Player
+import pickle
+
+server = "25.89.203.122"
+port = 4991
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	s.bind((server, port))
+except socket.error as e:
+	str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+players = [Player(player_x = WIDTH // 2, player_y = HEIGHT // 2, player_direction = 0, player_detector = 0), Player(player_x = WIDTH // 2 - 20, player_y = HEIGHT // 2 -20, player_direction = 0, player_detector = 0)]
+
+def threaded_client(conn, player):
+	conn.send(pickle.dumps(players[player]))
+	reply = ""
+	while True:
+		try:
+			data = pickle.loads(conn.recv(2048))
+			players[player] = data
+
+			if not data:
+				print("Disconnnected")
+				break
+			else:
+				if player == 1:
+					reply = players[0]
+				else:
+					reply = players[1]
+					print("Received: ", data)
+					print("Sending: ", reply)
+
+			conn.sendall(pickle.dumps(reply))
+		except:
+			break
+	print("Lost connection")
+	conn.close()
+
+currentPlayer = 0
+while True:
+	conn, addr = s.accept()
+	print("Connected to:", addr)
+
+	start_new_thread(threaded_client, (conn, currentPlayer))
+	currentPlayer += 1
