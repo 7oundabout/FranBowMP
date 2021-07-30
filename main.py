f574dc7f11d55b8b52d0e309bf4dda63c40d6c50
NVORON <85389911+CYBER-NVORON@users.noreply.github.com>
2021-07-30 13:46:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+from settings import *
+from network import Network
+from player import Player
+
+pygame.init()
+pygame.mixer.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('FBMP')
+
+
+background = pygame.image.load('backgrounds//1.png')
+#switch = Switch()
+
+n = Network()
+p = n.getP()
+clock = pygame.time.Clock()
+
+running = True
+while running:
+	clock.tick(FPS)
+	p2 = n.send(p)
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	sc.fill(SKY)
+	sc.blit(background, [0,0])
+#	switch.draw()
+	p.move(sc)
+	p2.draw(sc)
+	pygame.display.flip()
+
+
+pygame.quit()
