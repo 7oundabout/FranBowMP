c0648f56102c412e7cf1f148ce5fc99e186c28c5
7ound <7oundabout@gmail.com>
2021-08-11 07:39:05 +0300

    fixed

--- objects.py
+++ objects.py
@@ -11,10 +11,3 @@ class Platform(pygame.sprite.Sprite):
 		self.image = pygame.Surface((platform_height, platform_width))
 		self.image = platform_texture
 		self.rect = pygame.Rect(platform_x, platform_y, platform_width, platform_height)
-
-		
-class World_border(pygame.sprite.Sprite):
-	def __init__(self, width, height, x, y):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.Surface(())
-		self.rect = pygame.Rect(x, y, width, height)
