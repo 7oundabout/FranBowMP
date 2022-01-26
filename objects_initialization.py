eca75f71dbd1a9437de2084f4f475c644d4e0048
7oundabout <7oundabout@gmail.com>
2022-01-26 15:49:10 +0300

    Убрал ненужную переменную

--- objects_initialization.py
+++ objects_initialization.py
@@ -1,18 +1,15 @@
 import pygame
 from objects import *
 
-platforms = [""]
 platform_group = pygame.sprite.Group() 
 
 class objects_initialization():
 	def __init__(self):
-		self.pf_x = 0
-		self.pf_y = 0
+		self.pf_x = 0 ; self.pf_y = 0
 		self.levels_map = None
 
 	def platform_init(self, lvl): #функция инициализации платформ
 		platform_group.empty() #сброс platform_group
-		platforms.clear() #очистка листа
 		self.pf_x = 0 ; self.pf_y = 0 #сброс pf_x, pf_y - позиций для инициализации
 		
 		for line in self.levels_map[lvl]:
@@ -20,7 +17,6 @@ class objects_initialization():
 				if symbol == "1":
 					pf = Platform(self.pf_x, self.pf_y)
 					platform_group.add(pf)
-					platforms.append(pf)
 				self.pf_x += platform_width
 			self.pf_y += platform_height
 			self.pf_x = 0
